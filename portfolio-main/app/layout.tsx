ae87539d82ed44b2ac4e57f96cf56516d678db5d
prashanth2612 <prashanthit8074@gmail.com>
2024-06-17 18:25:37 +0530

    First Commit

---
+++ portfolio-main/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Prashanths's Portfolio",
+  description: "Empowering Tommorow",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ portfolio-main/data/index.ts
@@ -0,0 +1,148 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I value strong client partnerships and ensure transparency through consistent, open dialogue",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "https://th.bing.com/th/id/OIP.21kC6qfvFDY9WeNz5dcbDwHaFj?rs=1&pid=ImgDetMain",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building Medical LLM",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Revolutionize Agriculture with Smart Tech Rentals",
+    des: "Discover the future of farming with our innovative AgriTech platform, offering advanced machinery rentals and modern agricultural solutions. Explore cutting-edge technology designed to boost productivity and sustainability in agriculture.",
+    img: "https://th.bing.com/th/id/OIP.z27TG1KF2Pvnq9NDZi7BDwHaER?rs=1&pid=ImgDetMain",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Image Steganography",
+    des: "Unlock the power of secure communication with our advanced image steganography tools. Seamlessly embed and encrypt your sensitive information within images, ensuring privacy and protection against unauthorized access. Experience state-of-the-art technology that keeps your data hidden yet accessible",
+    img: "https://th.bing.com/th/id/OIP.1_seg-IqXsYwM0zxRtLwCQHaE6?rs=1&pid=ImgDetMain",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Vehicle Scratch Detector",
+    des: "Leverage cutting-edge machine learning technology to detect and analyze vehicle scratches with high precision. Our advanced system identifies even the smallest imperfections, ensuring accurate assessments and efficient solutions for vehicle maintenance and repair. Experience the future of automotive care with our intelligent scratch detection tool",
+    img: "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters:format(jpg)/https://specials-images.forbesimg.com/imageserve/1179697528/0x0.jpg%3FcropX1%3D0%26cropX2%3D7680%26cropY1%3D0%26cropY2%3D4320",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Healthify",
+    des: "Healthify is a modern web application that enhances healthcare interactions through advanced AI integration. It enables patients to receive personalized medical advice based on symptom descriptions, schedule appointments conveniently, and securely manage their medical records. Healthcare providers benefit from efficient report analysis, streamlined patient management, and secure communication tools. Healthify transforms healthcare delivery by prioritizing ease of use, efficiency, and patient empowerment.",
+    img: "https://th.bing.com/th/id/OIP.k-yScKhnWYUz79dn4DhPuQEsC7?rs=1&pid=ImgDetMain",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+
+
+export const companies = [
+  {
+    id: 1,
+    name: "Supraja Technologies",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Cyber Security Intern",
+    desc: "Assisted in the Cyber Security and Ethical Hacking.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev ",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
